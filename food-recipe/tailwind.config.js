990cc1cf8880bbca84d3422a912ccd64f8fddd7b
habrev <samuelgashu11@gmail.com>
2024-07-04 16:33:33 +0300

    last commited

--- food-recipe/tailwind.config.js
+++ food-recipe/tailwind.config.js
@@ -3,8 +3,24 @@ export default {
   content: ["./index.html", './src/**/*.{vue,js,ts,jsx,tsx}',
   ]
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      "weateher-primary": "#00668A",
+      "weather-secondary": "#004E71",
+      },
+    },
+    fontfamily:{
+      roboto: ["Roboto", "sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens:{
+      sm:"640px",
+      md:"768px",
+    },
   },
   plugins: [],
-}
+};
 
